4e25ac8c7c145da5b28b5e8fa6119545dd327a3e
Audun Solemdal <audun@solom.no>
2023-10-15 14:43:37 +0200

    Create denyaction-assignments.tf

---
+++ terraform/policy/denyaction-assignments.tf
@@ -0,0 +1,54 @@
+### Ensure none of these resources can be removed anywhere in the platform
+
+resource "azurerm_management_group_policy_assignment" "deny_actions" {
+  name                 = "denyactions"
+  display_name         = "[GOV] Disallow deletion of specific resource types"
+  description          = "Avoid accidental or malicious deletion of resources by disallowing deletion of specific resource types."
+  policy_definition_id = azurerm_policy_definition.policy_definitions["denyactions-nodelete"].id
+  management_group_id  = "yourmggroupidhere"
+  not_scopes = []
+  enforce = true
+
+  parameters = jsonencode({
+    "protectedResources" : {
+      "value" : [
+        "Microsoft.RecoveryServices/vaults",
+        "Microsoft.ContainerService/managedClusters",
+        "Microsoft.ContainerRegistry/registries",
+        "Microsoft.Databricks/workspaces",
+        "Microsoft.OperationalInsights/workspaces",
+        "Microsoft.Network/publicIPAddresses",
+        "Microsoft.Network/applicationGateways",
+        "Microsoft.Network/virtualWans",
+        "Microsoft.Network/virtualHubs",
+        "Microsoft.Network/vpnSites",
+        "Microsoft.Network/azureFirewalls",
+        "Microsoft.Network/firewallPolicies",
+        "Microsoft.Network/privateDnsZones",
+        "Microsoft.Network/dnsZones"
+      ]
+    }
+  })
+}
+
+### Ensure no SQL LTR backups can be removed
+
+resource "azurerm_management_group_policy_assignment" "deny_actions_all" {
+  name                 = "denyactions-all"
+  display_name         = "[GOV] Disallow deletion of specific resource types in ALL mode"
+  description          = "Avoid accidental or malicious deletion of resources by disallowing deletion of specific resource types."
+  policy_definition_id = azurerm_policy_definition.policy_definitions["denyactions-nodelete-all"].id
+  management_group_id  = "yourmggroupid2here"
+  not_scopes = []
+  enforce = true
+
+  parameters = jsonencode({
+    "protectedResources" : {
+      "value" : [
+        # SQL Server LTR backups are still in place even if the SQL Server and resoruce group is deleted
+        "Microsoft.Sql/locations/longTermRetentionServers/longTermRetentionDatabases/longTermRetentionBackups",
+        "Microsoft.Sql/locations/longTermRetentionManagedInstances/longTermRetentionDatabases/longTermRetentionManagedInstanceBackups",
+      ]
+    }
+  })
+}
